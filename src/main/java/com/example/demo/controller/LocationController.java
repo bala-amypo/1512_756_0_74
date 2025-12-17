14e26a6dd4b1dcac39ce3e21b8108d44cbb51de4
bala-amypo <bala@amypo.in>
2025-12-17 08:50:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -6,19 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.LocationEntity;
 import com.example.demo.service.LocationService;
 
+@RestController
 public class LocationController {
     @Autowired
-    LocationService LocationService;
+    private LocationService locationService;
+
     @PostMapping("/addlocation")
-    public LocationEntity add(@RequestBody LocationEntity le){
-        return LocationService.createlocation(le);
+    public LocationEntity add(@RequestBody LocationEntity le) {
+        return locationService.createlocation(le);
     }
-@GetMapping("/showlocation")
-public List<LocationEntity> show(){
-    return LocationService.getalllocation();
-} 
+
+    @GetMapping("/showlocation")
+    public List<LocationEntity> show() {
+        return locationService.getalllocation();
+    }
 }
--- src/main/java/com/example/demo/service/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/LocationServiceImpl.java
@@ -3,18 +3,23 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.LocationEntity;
 import com.example.demo.repository.LocationRepository;
 
+@Service
 public class LocationServiceImpl implements LocationService {
     @Autowired
-    LocationRepository Locationrepo;
+    private LocationRepository locationRepo;
+
     @Override
-    public LocationEntity createlocation(LocationEntity Le){
-        return Locationrepo.save(Le);
-    public List<LocationEntity> getall(){
-        return Locationrepo findAll();
+    public LocationEntity createlocation(LocationEntity le) {
+        return locationRepo.save(le);
     }
+
+    @Override
+    public List<LocationEntity> getalllocation() {
+        return locationRepo.findAll();
     }
 }
